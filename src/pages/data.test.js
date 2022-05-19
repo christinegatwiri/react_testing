c3fea6c8346e5644dc87a66e9de3f5b1ee49aa09
christinegatwiri <christinegatwiri18@gmail.com>
2022-05-19 20:48:20 +0300

    added jest string,array, object test

--- src/pages/data.test.js
+++ src/pages/data.test.js
@@ -9,3 +9,31 @@ test('Number of item should be two', () => {
 test('Number of item should be greater than 1', () => {
     expect(numItems).toBeGreaterThan(1);
 });
+
+const dataTest = data[0].title;
+
+test('there is a react in this title', () => {
+    expect(dataTest).toMatch(/react/);
+});
+
+test('there is a js in this title', () => {
+    expect(dataTest).toMatch(/Building/);
+});
+
+// arrays
+
+const data2 = ['Reactjs', 'React Native'];
+
+test('the list of courses contains react and django', () => {
+    expect(['Reactjs', 'React Native', 'Nativejs']).toEqual(expect.arrayContaining(data2));
+});
+
+// objects
+
+test('the first course to have a dict name title', () => {
+    expect(data[0]).toHaveProperty('title');
+});
+
+test('the first course to have a dict value django', () => {
+    expect(data[1]).toHaveProperty('category', 'django');
+});
